b1eaca93f2ca432dba27c3b5fa61273e5bcff612
edosyhptra <edo23syahputra@gmail.com>
2025-04-24 16:11:17 +0700

    update and delete

--- src/app_blog/urls.py
+++ src/app_blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from app_blog.views import create, list, read
+from app_blog.views import create, list, read, update, delete
 
 urlpatterns = [
     path('create/', create.view, name='app_blog'),
     path('list/', list.view, name='list_blog'),
     path('read/<int:id>/', read.view, name='read_blog'),
+    path('update/<int:id>/', update.view, name='blog_update'),
+    path('delete/<int:id>/', delete.view, name='blog_delete'),
 ]
